bafcb2a7c72a0f151f1f08c04e432ce3196965f3
xXCooBloyXx <89714046+xXCooBloyXx@users.noreply.github.com>
2022-12-18 22:25:03 +0100

    1.1

--- useful.py
+++ useful.py
@@ -4,7 +4,7 @@ bl_info = {
     "name": "Useful BS",
     "description": "Addon with useful tools for Brawl Stars Editing",
     "author": "xXCooBloyXx",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (3, 0, 0),
     "location": "View3D > Useful BS",
     "category": "Object"
@@ -29,6 +29,12 @@ class UsefulPanel(bpy.types.Panel):
         row = layout.row()
         row.operator("object.fix_names", text="Fix Names")
 
+        row = layout.row()
+        row.label(text="UV Tools:")
+
+        row = layout.row()
+        row.operator("object.fix_glb_uv", text="Fix Glb UV")
+
 class FixUVNamesOperator(bpy.types.Operator):
     bl_idname = "object.fix_uv_names"
     bl_label = "Fix UV Names"
@@ -52,15 +58,37 @@ class FixNamesOperator(bpy.types.Operator):
                 obj.data.name = obj.name+"-mesh"
         return {'FINISHED'}
 
+class FixGlbUVOperator(bpy.types.Operator):
+    bl_idname = "object.fix_glb_uv"
+    bl_label = "Fix Glb UV"
+    bl_description = "Fixes UV from glb models"
+
+    def execute(self, context):
+
+        bpy.ops.object.editmode_toggle()
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.context.area.ui_type = 'UV'
+        bpy.context.space_data.pivot_point = 'CURSOR'
+        bpy.ops.uv.select_all(action='SELECT')
+        bpy.ops.transform.resize(value=(0.000244, 0.000244, 0.000244), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        bpy.ops.transform.translate(value=(0, 1, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        bpy.context.area.ui_type = 'VIEW_3D'
+        bpy.ops.object.editmode_toggle()
+		
+        return {'FINISHED'}
+
 def register():
     bpy.utils.register_class(UsefulPanel)
     bpy.utils.register_class(FixUVNamesOperator)
     bpy.utils.register_class(FixNamesOperator)
+    bpy.utils.register_class(FixGlbUVOperator)
 
 def unregister():
     bpy.utils.unregister_class(UsefulPanel)
     bpy.utils.unregister_class(FixUVNamesOperator)
     bpy.utils.unregister_class(FixNamesOperator)
+    bpy.utils.unregister_class(FixGlbUVOperator)
+
 
 if __name__ == "__main__":
     register()
